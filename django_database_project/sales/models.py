3382e4bdaf725c81d84ab339c84d9ddc32cdf9a6
Simeon199 <sim.ki@outlook.de>
2025-09-04 18:14:09 +0200

    feat: relationships added for testing purposes

--- django_database_project/sales/models.py
+++ django_database_project/sales/models.py
@@ -8,3 +8,34 @@ class Customer(models.Model):
     newsletter_abo = models.BooleanField(default=True)
     email_address = models.CharField(max_length=30, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name}({self.price})"
+
+
+class Bill(models.Model):
+    total_amount = models.FloatField()
+    is_paid = models.BooleanField(default=False)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through="Producttype")
+    bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+
+
+class Producttype(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    type_name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.type_name}"
