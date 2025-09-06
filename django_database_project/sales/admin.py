9169fd50da7f2bcd65d8e23a084e1aa3a85b6573
Simeon199 <sim.ki@outlook.de>
2025-09-06 16:42:43 +0200

    feat: experimenting with models.py and admin.py

--- django_database_project/sales/admin.py
+++ django_database_project/sales/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Bill, Product, Producttype, Order
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter = ["first_name", "last_name"]
+    readonly_fields = ["account"]
+
+    prepopulated_fields = {"slug":["first_name", "last_name"]}
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Bill)
+admin.site.register(Product)
+admin.site.register(Producttype)
+admin.site.register(Order)
